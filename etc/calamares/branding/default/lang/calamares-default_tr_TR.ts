be2d86b7203c7e52973b237416e2f02335816190
mustafa kılıç <63645922+mustafakilic@users.noreply.github.com>
2020-06-28 00:45:41 +0300

    Add files via upload

---
+++ etc/calamares/branding/default/lang/calamares-default_tr_TR.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>show</name>
+    <message>
+        <location filename="../show.qml" line="64"/>
+        <source>Eski Linux bir linux dağıtımıdır.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../show.qml" line="68"/>
+        <source>Eski Linux Arch Linux tabanlıdır.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
